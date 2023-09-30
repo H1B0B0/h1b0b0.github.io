94419e0c7e476105283fadf43a106c3bed38e5ca
etienne mentrel <etienne.mentrel@epitech.eu>
2023-09-30 18:55:28 +0200

    yml and astro config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://h1b0b0.github.io',
+    site: 'https://h1b0b0.github.io/',
 });
