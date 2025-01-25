1847c823f877c03aa39e051c04bfff8d3fd8e390
H1B0B0 <hibosmurf@gmail.com>
2025-01-25 20:12:55 +0100

    Add React integration and new SplineWrapper component; enhance animations in Skills and Hero components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
-    site: 'https://h1b0b0.github.io/',
-    base: "/",
+  site: "https://h1b0b0.github.io/",
+  base: "/",
+  integrations: [react()],
 });
